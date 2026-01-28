eba78e6159d2ae0ed013579961a556a700536ac0
vivianazunigaolavarria <vizuol21@gmail.com>
2026-01-28 15:56:19 -0600

    Add loops and counters practice

---
+++ loops_and_counters.py
@@ -0,0 +1,16 @@
+# Practice: loops and counters
+
+# FOR loop example
+print("For loop:")
+for i in range(5):
+    print("Iteration:", i)
+
+# WHILE loop example
+print("\nWhile loop:")
+counter = 5
+
+while counter > 0:
+    print("Counter:", counter)
+    counter -= 1
+
+print("Done!")
